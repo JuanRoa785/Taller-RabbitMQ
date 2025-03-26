f6fb2edf193fdec583c8183752a36a2ce80fc2f7
roa <roaporrasjuandiego@gmail.com>
2025-03-26 14:30:12 -0500

    deleting an unnecesary comment

--- back/python/main.py
+++ back/python/main.py
@@ -17,7 +17,5 @@ app.add_middleware(
 app.include_router(consumer_router, prefix="/consumer")
 app.include_router(publisher_router, prefix="/publisher")
 
-#EndPoint Para consumir mensajes:
-
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
